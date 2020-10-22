1c8fbf4e7b2f1b5ae768df2eccff669de2aa2497
Crowdbotics <team@crowdbotics.com>
2020-10-22 17:28:15 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/flowchart_21835/MainActivity.java
@@ -0,0 +1,15 @@
+package com.flowchart_21835;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "flowchart_21835";
+    }
+}
